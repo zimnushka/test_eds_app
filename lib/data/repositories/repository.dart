89cc471f0ffa34e66d14e8540a5b5dae688464a3
Кирилл Зимнухов <g741980@gmail.com>
2022-07-14 15:18:41 +0300

    Add files and main repository

---
+++ lib/data/repositories/repository.dart
@@ -0,0 +1,91 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+abstract class Repository {
+  static const homeUrl = "https://jsonplaceholder.typicode.com/";
+  static const int _countSecTimeOut = 10;
+  static const String _timeOutMessage = "Ошибка подключения к серверу";
+
+  static Future<ResponseData> postRequest({
+    required String url,
+    required String parametrs,
+    required Map<String, String> headers,
+  }) async {
+    try {
+      http.Response response = await http
+          .post(Uri.parse("$homeUrl$url"), headers: headers, body: parametrs)
+          .timeout(const Duration(seconds: _countSecTimeOut));
+
+      return ResponseData(
+        data: utf8.decode(response.bodyBytes),
+        status: response.statusCode,
+      );
+    } catch (e) {
+      return const ResponseData(data: _timeOutMessage, status: 408);
+    }
+  }
+
+  static Future<ResponseData> getRequest(
+    String url, {
+    Map<String, String>? headers,
+  }) async {
+    try {
+      http.Response response = await http
+          .get(Uri.parse("$homeUrl$url"), headers: headers)
+          .timeout(const Duration(seconds: _countSecTimeOut));
+
+      return ResponseData(
+        data: utf8.decode(response.bodyBytes),
+        status: response.statusCode,
+      );
+    } catch (e) {
+      return const ResponseData(data: _timeOutMessage, status: 408);
+    }
+  }
+
+  static Future<ResponseData> putRequest({
+    required String url,
+    required String parametrs,
+    required Map<String, String> headers,
+  }) async {
+    try {
+      http.Response response = await http
+          .put(Uri.parse("$homeUrl$url"), headers: headers, body: parametrs)
+          .timeout(const Duration(seconds: _countSecTimeOut));
+
+      return ResponseData(
+        data: utf8.decode(response.bodyBytes),
+        status: response.statusCode,
+      );
+    } catch (e) {
+      return const ResponseData(data: _timeOutMessage, status: 408);
+    }
+  }
+
+  static Future<ResponseData> deleteRequest({
+    required String url,
+    required Map<String, String> headers,
+  }) async {
+    try {
+      http.Response response = await http
+          .delete(Uri.parse("$homeUrl$url"), headers: headers)
+          .timeout(const Duration(seconds: _countSecTimeOut));
+
+      return ResponseData(
+        data: utf8.decode(response.bodyBytes),
+        status: response.statusCode,
+      );
+    } catch (e) {
+      return const ResponseData(data: _timeOutMessage, status: 408);
+    }
+  }
+}
+
+class ResponseData {
+  final String data;
+  final int? status;
+  final bool? isSuccesful;
+
+  const ResponseData({required this.data, this.status, this.isSuccesful});
+}
