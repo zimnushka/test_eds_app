660d546f9085d46c49444eee05992f67a9006a97
Кирилл Зимнухов <g741980@gmail.com>
2022-07-15 02:39:18 +0300

    Add comment page/ comment editor

--- lib/data/repositories/comments_repository.dart
+++ lib/data/repositories/comments_repository.dart
@@ -5,8 +5,8 @@ import 'package:test_eds_app/data/repositories/repository.dart';
 import '../models/comment_model.dart';
 
 class CommentsRepository extends Repository {
-  Future<List<Comment>> getComments() async {
-    final res = await getRequest("comments", headers: {});
+  Future<List<Comment>> getComments(int postId) async {
+    final res = await getRequest("posts/$postId/comments", headers: {});
     if (res.status == 200) {
       List objects = jsonDecode(res.data);
       return objects.map((e) => Comment.fromJson(e)).toList();
@@ -14,10 +14,14 @@ class CommentsRepository extends Repository {
     return [];
   }
 
-  Future<Comment?> getComment(int id) async {
-    final res = await getRequest("comments/$id", headers: {});
+  Future<Comment?> sendComment(Comment comment) async {
+    final res = await postRequest(
+        url: "comments",
+        headers: {'Content-type': 'application/json; charset=UTF-8'},
+        parametrs: jsonEncode(comment.toJson));
+    print(res.data);
     if (res.status == 200) {
-      return Comment.fromJson(jsonDecode(res.data));
+      return comment;
     }
     return null;
   }
--- lib/ui/pages/post_pages/post_page.dart
+++ lib/ui/pages/post_pages/post_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:test_eds_app/data/models/comment_model.dart';
+import 'package:test_eds_app/data/repositories/comments_repository.dart';
+import 'package:test_eds_app/ui/widgets/comment_widgets/comment_card.dart';
+import 'package:test_eds_app/ui/widgets/comment_widgets/comment_editor.dart';
 
 import '../../../data/models/post_model.dart';
 
@@ -11,24 +15,58 @@ class PostPage extends StatefulWidget {
 }
 
 class _PostPageState extends State<PostPage> {
+  final CommentsRepository repository = CommentsRepository();
+
+  void addComment() async {
+    await showModalBottomSheet(
+        backgroundColor: Theme.of(context).cardColor,
+        context: context,
+        isScrollControlled: true,
+        builder: (context) {
+          return CommentEditor(
+            postId: widget.post.id,
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(elevation: 0),
-      body: CustomScrollView(slivers: [
-        SliverToBoxAdapter(
-          child: Container(
-            color: Theme.of(context).cardColor,
-            child: ListTile(
-              title: Text(widget.post.title),
-              subtitle: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 10),
-                child: Text(widget.post.body),
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Container(
+              color: Theme.of(context).cardColor,
+              child: ListTile(
+                contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+                title: Text(widget.post.title),
+                subtitle: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(widget.post.body),
+                ),
               ),
             ),
           ),
-        )
-      ],),
+          FutureBuilder<List<Comment>>(
+              future: repository.getComments(widget.post.id),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                      (BuildContext context, int index) {
+                        return CommentCard(comment: snapshot.data![index]);
+                      },
+                      childCount: snapshot.data!.length,
+                    ),
+                  );
+                }
+                return SliverToBoxAdapter(child: LinearProgressIndicator());
+              })
+        ],
+      ),
+      floatingActionButton:
+          FloatingActionButton(onPressed: addComment, child: Icon(Icons.chat)),
     );
   }
 }
