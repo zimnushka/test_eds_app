9d057ec9e8ec6096f59acdaa05ee8cfaadd8a42f
Кирилл Зимнухов <g741980@gmail.com>
2022-07-14 17:28:58 +0300

    add photo model

---
+++ lib/data/models/photo_model.dart
@@ -0,0 +1,24 @@
+class Photo {
+  final int albumId;
+  final int id;
+  final String title;
+  final String url;
+  final String thumbnailUrl;
+
+  const Photo(
+      {required this.albumId,
+      required this.id,
+      required this.thumbnailUrl,
+      required this.title,
+      required this.url});
+
+  static Photo fromJson(Map<String, dynamic> json) {
+    return Photo(
+      albumId: json["albumId"],
+      id: json["id"],
+      thumbnailUrl: json["thumbnailUrl"],
+      title: json["title"],
+      url: json["url"],
+    );
+  }
+}
