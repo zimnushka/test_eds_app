86b42faba6514c9f432d34ac3cb719168bc72f66
Кирилл Зимнухов <kirillzimnuhov@Mac-mini-Kirill.local>
2022-07-15 11:30:10 +0300

    Add SQlite

--- lib/data/repositories/users_repository.dart
+++ lib/data/repositories/users_repository.dart
@@ -3,6 +3,9 @@ import 'dart:convert';
 import 'package:test_eds_app/data/models/user_model.dart';
 import 'package:test_eds_app/data/repositories/repository.dart';
 
+import '../models/album_model.dart';
+import '../models/post_model.dart';
+
 class UsersRepository extends Repository {
   Future<List<User>> getUsers() async {
     final res = await getRequest("users", headers: {});
@@ -13,11 +16,21 @@ class UsersRepository extends Repository {
     return [];
   }
 
-  Future<User?> getUser(int id) async {
-    final res = await getRequest("users/$id", headers: {});
+  Future<List<Post>> getUserPosts(int userId) async {
+    final res = await getRequest("users/$userId/posts", headers: {});
     if (res.status == 200) {
-      return User.fromJson(jsonDecode(res.data));
+      List objects = jsonDecode(res.data);
+      return objects.map((e) => Post.fromJson(e)).toList();
     }
-    return null;
+    return [];
+  }
+
+  Future<List<Album>> getUserAlbums(int userId) async {
+    final res = await getRequest("users/$userId/albums", headers: {});
+    if (res.status == 200) {
+      List objects = jsonDecode(res.data);
+      return objects.map((e) => Album.fromJson(e)).toList();
+    }
+    return [];
   }
 }
--- lib/ui/pages/user_pages/user_page.dart
+++ lib/ui/pages/user_pages/user_page.dart
@@ -1,7 +1,12 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:test_eds_app/data/models/album_model.dart';
+import 'package:test_eds_app/data/models/post_model.dart';
 import 'package:test_eds_app/data/repositories/users_repository.dart';
 import 'package:test_eds_app/ui/pages/album_pages/album_list_page.dart';
 import 'package:test_eds_app/ui/pages/post_pages/post_list_page.dart';
+import 'package:test_eds_app/ui/widgets/album_widget/album_card.dart';
+import 'package:test_eds_app/ui/widgets/post_widgets/post_card.dart';
 import 'package:test_eds_app/ui/widgets/user_widgets/company_card.dart';
 import 'package:test_eds_app/ui/widgets/user_widgets/user_contact_info.dart';
 
@@ -37,30 +42,100 @@ class _UserPageState extends State<UserPage> {
               item: widget.user.company,
               address: widget.user.address,
             ),
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return PostsListPage(userId: widget.user.id);
+            ListTile(
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Posts"),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return PostsListPage(userId: widget.user.id);
+                          },
+                        ),
+                      );
                     },
+                    child: const Text("more"),
                   ),
-                );
-              },
-              child: const Text("POSTS"),
+                ],
+              ),
+              subtitle: FutureBuilder<List<Post>>(
+                  future: repository.getUserPosts(widget.user.id),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return CarouselSlider(
+                        options: CarouselOptions(
+                            height: 100,
+                            aspectRatio:
+                                MediaQuery.of(context).size.width / 100),
+                        items: [
+                          PostCard(
+                            item: snapshot.data![0],
+                            onTap: () {},
+                          ),
+                          PostCard(
+                            item: snapshot.data![1],
+                            onTap: () {},
+                          ),
+                          PostCard(
+                            item: snapshot.data![2],
+                            onTap: () {},
+                          ),
+                        ],
+                      );
+                    }
+                    return LinearProgressIndicator();
+                  }),
             ),
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return AlbumsListPage(userId: widget.user.id);
+            ListTile(
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Albums"),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return AlbumsListPage(userId: widget.user.id);
+                          },
+                        ),
+                      );
                     },
+                    child: const Text("more"),
                   ),
-                );
-              },
-              child: const Text("ALBUMS"),
-            )
+                ],
+              ),
+              subtitle: FutureBuilder<List<Album>>(
+                  future: repository.getUserAlbums(widget.user.id),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return CarouselSlider(
+                        options: CarouselOptions(
+                            height: 100,
+                            aspectRatio:
+                                MediaQuery.of(context).size.width / 100),
+                        items: [
+                          AlbumCard(
+                            item: snapshot.data![0],
+                            onTap: () {},
+                          ),
+                          AlbumCard(
+                            item: snapshot.data![1],
+                            onTap: () {},
+                          ),
+                          AlbumCard(
+                            item: snapshot.data![2],
+                            onTap: () {},
+                          ),
+                        ],
+                      );
+                    }
+                    return LinearProgressIndicator();
+                  }),
+            ),
           ],
         ),
       ),
